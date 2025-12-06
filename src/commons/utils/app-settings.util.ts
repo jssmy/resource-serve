7e7d951ce6e091176869f657a8187b6b4130e941
Joset Manihuari <jssmy@Josets-MacBook-Air.local>
2025-12-06 14:39:02 -0500

    feat: add additional app settings properties
    - Add APP_EMAIL, APP_URL, APP_SUPPORT_EMAIL, APP_SUPPORT_URL to AppSettings
    - Extend AppSettings utility class with email and URL configurations

--- src/commons/utils/app-settings.util.ts
+++ src/commons/utils/app-settings.util.ts
@@ -1,3 +1,7 @@
 export class AppSettings {
-  static readonly APP_NAME = 'BugZilo';
+    static readonly APP_NAME = 'BugZilo';
+    static readonly APP_EMAIL = 'bugzilo.team@gmail.com';
+    static readonly APP_URL = 'https://bugzilo.com';
+    static readonly APP_SUPPORT_EMAIL = 'bugzilo.team@gmail.com';
+    static readonly APP_SUPPORT_URL = 'https://bugzilo.com';
 }
