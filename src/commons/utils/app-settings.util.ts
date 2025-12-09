7eb47653678a130eff5c3e6d7c1ef753b53f788c
Joset Manihuari <jssmy@Josets-MacBook-Air.local>
2025-12-08 21:48:28 -0500

    chore: Limpiar línea vacía en app-settings.util.ts

--- src/commons/utils/app-settings.util.ts
+++ src/commons/utils/app-settings.util.ts
@@ -3,5 +3,4 @@ export class AppSettings {
   static readonly APP_EMAIL = 'bugzilo.team@gmail.com';
   static readonly APP_URL = 'https://bugzilo.com';
   static readonly APP_SUPPORT_EMAIL = 'bugzilo.team@gmail.com';
-  
 }
